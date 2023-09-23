3195ab2aefbd39a1ecb58b9c8e33e457919a00d0
LilyAnika <rbagchee@gmail.com>
2023-09-23 18:32:48 -0400

    After fixing all errors

--- src/app/components/recipe-summary/recipe-summary.component.ts
+++ src/app/components/recipe-summary/recipe-summary.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe';
 
 @Component({
   selector: 'app-recipe-summary',
   templateUrl: './recipe-summary.component.html',
   styleUrls: ['./recipe-summary.component.css']
 })
-export class RecipeSummaryComponent {
 
+export class RecipeSummaryComponent {
+  @Input()
+  recipe: Recipe;
+
+  constructor(){}
+  
 }
